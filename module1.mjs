74fb233649a1b811037506fd8743c09099dd08e1
zubair <z9596318@gmail.com>
2024-06-26 14:01:09 +0500

    Updates

--- module1.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Zubair";
-const b = "Mehmood";
-const c = "Qarnain";
-const d = "Awan";
-
-export default a;
-export {b};
-export {c};
-export {d};
--- module2.mjs
+++
@@ -1,5 +0,0 @@
-import a, {b , c, d} from'./module1.mjs'
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
